e418c03330ac791b83f3bf9a7e0d20c4e0bf1a7d
arpitsha26 <arpitsharma1263@gmail.com>
2026-08-26 23:13:31 +0530

    feat: Setup Redis

--- backend/services/auth/controllers/auth.controller.js
+++ backend/services/auth/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 
-import { getAuth } from "firebase-admin/auth"
+import { getAuth, UserInfo } from "firebase-admin/auth"
 import { app } from "../config/firebase.js"
 import User from "../models/user.model.js"
+import redis from "../../../shared/redis/redis.js"
 
 export const login=async(req,res)=>{
 
@@ -27,6 +28,13 @@ export const login=async(req,res)=>{
 
         const sessionId=crypto.randomUUID();
 
+        await redis.set(`session-${sessionId}`,JSON.stringify({
+            userId:user._id,
+            name:user.name,
+            email:user.email,
+            avatar:user.avatar
+        }),"EX",7*24*60*60)
+
         res.cookie("session", sessionId,{
                    httpOnly:true,
                    secure:false,     //after deplpy isko true kr dena h
@@ -42,6 +50,31 @@ export const login=async(req,res)=>{
 
         
         
+    } catch (error) {
+
+        return res.status(500).json({message:`login error ${error}`})
+
+
+        
+    }
+
+}
+
+
+export const logOut=async(req,res)=>{
+
+    try {
+        
+        const sessionId=req.cookies?.session
+
+        await redis.del(`session-${sessionId}`)
+
+        res.clearCookie("session")
+
+        return res.status(200).json({message:"logout successfully"})
+        
+
+
     } catch (error) {
 
         return res.status(500).json({message:`login error ${error}`})
